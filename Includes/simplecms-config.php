f8ddcfba76789ced3aacf68fcbfc2291e340951e
karegakala <karega64@gmail.com>
2014-11-08 16:09:12 -0400

    Intialized Project
    added website files

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'serviceDeskDB');
+    define('DB_USER', 'b53592030c29ac');
+    define('DB_PASSWORD', '8fae7862');
+    define('DB_HOST', 'us-cdbr-azure-east2-d.cloudapp.net');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', 'admin');
+?>
